499d281628933be568f2054d73ab22ad8b257548
Michael Lange <mlange.cs@gmail.com>
2013-04-17 23:39:56 -0700

    brings my personal friends list to the dashboard

--- app.js
+++ app.js
@@ -4,33 +4,42 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+  , routes  = require('./routes')
+  , http    = require('http')
+  , path    = require('path');
 
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io');
 
 app.configure(function(){
+  
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function (){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+var sio = io.listen(server);
+
+sio.sockets.on('connection', function (socket) {
+  console.log('Socket Connection');
+});
+
+app.get('/', routes.index(sio));
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,82 @@
-var LastFmNode = require('lastfm').LastFmNode;
-var config = require('../config');
+var LastFmNode = require('lastfm').LastFmNode
+  , config = require('../config')
+  , moment = require('moment');
 
-console.log(config);
+var lastfm = new LastFmNode({
+  api_key:   config.lastfm.api_key,
+  secret:    config.lastfm.secret,
+  useragent: 'lastfm-streams/v0.1 Lastfm Streams'
+});
 
-var lastfm = new LastFmNode(config.lastfm);
-
-var test = lastfm.stream('DingoEatingFuzz');
+var test = lastfm.stream('dubsaru');
 var tracks = [];
 
 test.on('nowPlaying', function(track) {
-  console.log('foo');
-  console.log(track);
+  //console.log(track);
   tracks.push(track);
 });
 
 test.on('error', function(error) {
   console.log('something went wrong within lastfm module');
   console.log(error);
-})
+});
 
 test.start();
 
-exports.index = function(req, res) {
-  res.render('index', {
-    title: 'lastfm streams',
-    friends: [
-      {
-        name: 'Michael Lange',
-        tracks: tracks
-      }, {
-        name: 'Foo Bar',
-        tracks: [
-          { url: 'google.com', name: 'Animals' },
-          { url: '#', name: 'Pigs' },
-          { url: '#', name: 'Track Three' },
-          { url: '#', name: 'Track Four' },
-          { url: '#', name: 'Track Five' },
-        ]
+exports.index = function(io) {
+
+  var users = [];
+
+  lastfm.request('user.getFriends', {
+    user: 'DingoEatingFuzz',
+    handlers: {
+      success: function(data) {
+        users = data.friends.user;
+      },
+      failure: function(error) {
+        console.log('getFriends error: ', error.message);
       }
-    ]
+    }
   });
+
+  return function(req, res) {
+    buildLists(users, function(list) {
+      res.render('index', viewObj(list));
+    });
+  };
 };
+
+function buildLists(users, cb) {
+
+  if (!users.length) return [];
+
+  var completed = 0,
+      lists = [];
+  users.forEach(function(user) {
+    lastfm.request('user.getRecentTracks', {
+      user: user.name,
+      limit: 10,
+      from: moment().subtract('h', 1).unix(),
+      handlers: {
+        success: function(data) {
+          console.log('fetched tracks for: ', user.name);
+          completed++;
+          lists.push({
+            name: user.realname || user.name,
+            tracks: data.recenttracks.track || []
+          });
+          if (completed === users.length) {
+            cb(lists);
+          }
+        }
+      }
+    })
+  });
+}
+
+function viewObj(list) {
+  return {
+    title: 'lastfm streams',
+    friends: list
+  };
+}
